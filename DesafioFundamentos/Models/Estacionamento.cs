cc15bf1e5ece342d741082f02ddad9ad8b9981a4
Bruno Agnelo De Almeida <3714194@empresa.mti.mt.gov.br>
2025-10-02 10:05:55 -0400

    Desafio Concluídogit add trilha-net-fundamentos-desafio.sln

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -12,55 +12,124 @@ namespace DesafioFundamentos.Models
             this.precoPorHora = precoPorHora;
         }
 
+        // Função para adicionar veículo
+        //Implementado!!!
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string novoVeiculo = Console.ReadLine();
+
+            bool veiculoExiste = veiculos.Any(x => x.ToUpper() == novoVeiculo.ToUpper());
+            if (veiculoExiste)
+            {
+                Console.WriteLine("Veículo já está estacionado.");
+                Console.WriteLine("O cadastro foi cancelado.");
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                veiculos.Add(novoVeiculo);
+            }
+
+            if (veiculos.Contains(novoVeiculo))
+            {
+                Console.WriteLine("Veículo adicionado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Erro ao adicionar veículo.");
+            }
         }
 
+        // Função para remover veículo
+        //Implementado!!!
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
-
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
-
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            Console.WriteLine("Digite a placa do veículo para pesquisar:");
+            string placa = Console.ReadLine();
+
+            var veiculoEncontrado = veiculos.Where(x => x.ToUpper() == placa.ToUpper()).ToList();
+            if (veiculoEncontrado.Any())
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal valorTotal = precoInicial + (precoPorHora * horas);
+                veiculos.Remove(veiculoEncontrado[0]);
+                Console.WriteLine($"O veículo {veiculoEncontrado[0]} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine("Veículo não encontrado.");
             }
         }
 
+        // Função para listar veículos
+        //Implementado!!!
         public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+            bool menuListarVeiculos = true;
+
+            while (menuListarVeiculos)
             {
-                Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
-            }
-            else
-            {
-                Console.WriteLine("Não há veículos estacionados.");
+                Console.Clear();
+                Console.WriteLine("================Veículos Estacionados================");
+                Console.WriteLine("Digite a sua opção:");
+                Console.WriteLine("1 - Listar todos os veículos");
+                Console.WriteLine("2 - Listar veículos por placa");
+                Console.WriteLine("3 - Retornar ao menu principal");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        ListarTodosVeiculos();
+                        break;
+
+                    case "2":
+                        ListarVeiculosPorPlaca();
+                        break;
+                    case "3":
+                        menuListarVeiculos = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+
+
+                // Verifica se há veículos no estacionamento
+                void ListarTodosVeiculos()
+                {
+                    Console.WriteLine("Os veículos estacionados são:");
+                    foreach (var veiculo in veiculos)
+                    {
+                        Console.WriteLine(veiculo);
+                    }
+                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                    Console.ReadKey();
+                }
+
+                // Função para listar um veículo por placa
+                void ListarVeiculosPorPlaca()
+                {
+                    Console.WriteLine("Digite a placa do veículo para pesquisar:");
+                    string placa = Console.ReadLine();
+
+                    var veiculoEncontrado = veiculos.Where(x => x.ToUpper() == placa.ToUpper()).ToList();
+                    if (veiculoEncontrado.Any())
+                    {
+                        Console.WriteLine($"Veículo encontrado: {veiculoEncontrado[0]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Veículo não encontrado.");
+                    }
+
+                    Console.WriteLine("Pressione qualquer tecla para continuar...");
+                    Console.ReadKey();
+                }
             }
         }
     }
